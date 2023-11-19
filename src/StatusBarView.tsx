2489cfb7d4116e49edc05d14bc6d541aec0bc1c8
Andreas Kulicke <a.kulicke@gmx.de>
2023-11-19 16:21:51 +0100

    fix(screen): add padding to bottom for navigation bar

--- src/StatusBarView.tsx
+++ src/StatusBarView.tsx
@@ -2,7 +2,7 @@ import { SafeAreaView } from "react-native";
 
 export function StatusBarView(props: React.PropsWithChildren<{}>) {
     return (
-        <SafeAreaView style={{ flexGrow: 1, flexShrink: 1 }}>
+        <SafeAreaView style={{ flexGrow: 1, flexShrink: 1, paddingBottom: 46 }}>
             {props.children}
         </SafeAreaView>
     );
