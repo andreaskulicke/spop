98e79c460947870f68c192fdbe88a363f70ef50c
Andreas Kulicke <a.kulicke@gmx.de>
2023-12-27 18:27:53 +0100

    fix(price): shop price arrows only if for more than one shop

--- src/PriceIcon.tsx
+++ src/PriceIcon.tsx
@@ -1,4 +1,4 @@
-import { selectItemShopsWithMinPrice } from './store/dataSlice';
+import { selectItem, selectItemShopsWithMinPrice } from './store/dataSlice';
 import { useTheme, Icon } from 'react-native-paper';
 import { useAppSelector } from './store/hooks';
 import React from 'react';
@@ -7,16 +7,20 @@ export function PriceIcon(props: {
     itemId: string;
     shopId: string;
 }) {
+    const item = useAppSelector(selectItem(props.itemId));
     const itemShopsWithMinPrice = useAppSelector(selectItemShopsWithMinPrice(props.itemId));
     const theme = useTheme();
 
+    const numberOfShopsWithPrices = item?.shops.filter(x => x.price).length ?? 0;
     const hasMinPriceTmp = itemShopsWithMinPrice.find(x => x.shopId === props.shopId);
 
     return (
-        <Icon
-            color={hasMinPriceTmp ? theme.colors.primary : theme.colors.error}
-            source={hasMinPriceTmp ? "arrow-up" : "arrow-down"}
-            size={16}
-        />
+        (numberOfShopsWithPrices > 1)
+            ? <Icon
+                color={hasMinPriceTmp ? theme.colors.primary : theme.colors.error}
+                source={hasMinPriceTmp ? "arrow-up" : "arrow-down"}
+                size={16}
+            />
+            : <></>
     );
 }
