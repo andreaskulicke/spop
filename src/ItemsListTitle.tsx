6ed996df2d960c41691bb7db624e83f1fae24230
Andreas Kulicke <a.kulicke@gmx.de>
2025-08-29 09:24:46 +0200

    fix(items): text color in dark mode

--- src/ItemsListTitle.tsx
+++ src/ItemsListTitle.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { useTheme } from "react-native-paper";
 
 export function ItemsListTitle(props: { title: string }) {
+    const theme = useTheme();
+
     return (
         <View>
-            <Text style={{ fontSize: 16, maxHeight: 42 }}>{props.title}</Text>
+            <Text
+                style={{
+                    fontSize: 16,
+                    maxHeight: 42,
+                    color: theme.colors.onPrimaryContainer,
+                }}
+            >
+                {props.title}
+            </Text>
         </View>
     );
 }
